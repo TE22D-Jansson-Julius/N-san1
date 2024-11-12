941d69b160d1ec793c2fed8b2cc33df9e756eb0a
Julius Jansson <julius.jansson@elev.ga.ntig.se>
2024-11-12 10:14:29 +0100

    first commit

---
+++ Näsan1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Steg 1
+if(6>3){
+Console.WriteLine("Hello, World!");
+}
+
+// Steg 2
+string userName = Console.ReadLine();
+if (userName == "noname"){
+    Console.WriteLine("Welcome!");
+}
+
+// Steg 3
+string username2 = Console.ReadLine();
+string password = Console.ReadLine();
+if (userName == "noname" && password == "nopass"){
+    Console.WriteLine("Welcome!");
+}
+    else 
+    {
+        Console.WriteLine("Wrong username or password");
+    }
+
+// Steg 4
+for (int i = 0; i < 32; i++){
+    Console.WriteLine("Hello World");
+}
+
+// Steg 5
+string password2 ="";
+while (password2 != "nopass"){
+password2 = Console.ReadLine();
+}
